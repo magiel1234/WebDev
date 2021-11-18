fe2102623779bad1c10a6b7678a8a44eb6e7e1ce
magiel1234 <akrldpf@gmail.com>
2021-11-17 22:13:16 -0800

    Ch1, Ch2, Ch3, Ch4(working), Ch5(working)

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+var cartnum=0;
+var sort=0;  // 0: default, 1: price, 2: recent, 3: popular
+var sorted = ["120W UV LED", "Mexican Taco Maker", "Moscow Mule Mugs", "Dryer", "Weighted Blanket"];
+var sortedprice = [3,1,2,4,5];
+var totalprice=0;
+var cartnumoccupied = [0,0,0,0,0,0,0];
+
 function alert_button(str) {
     alert(str);
 }
@@ -39,4 +46,37 @@ function mOverh(obj) {
 
 function mOuth(obj) {
     obj.style.fontSize="20px";
+}
+
+function additem(num)
+{
+    cartnum++;
+    var a = document.getElementById('cartlist');
+    a.options[cartnum].innerText = sorted[num-1];
+    totalprice = totalprice + sortedprice[num-1];
+    cartnumoccupied[cartnum]=num;
+    console.log(cartnumoccupied);
+}
+
+function removeitem(num)
+{
+    
+    for(let aa=1;aa<7;aa++)
+    {
+        if(cartnumoccupied[aa]==num)
+        {
+            if(cartnum>0) 
+            {
+                cartnum--;
+                var a = document.getElementById('cartlist');
+                a.options[cartnum+1].innerText = "";
+                totalprice = totalprice - sortedprice[num-1];
+                cartnumoccupied[aa]=0;
+            }
+        }
+    }
+}
+
+function checkout(){
+    alert("Your total is "+totalprice+" $");
 }
